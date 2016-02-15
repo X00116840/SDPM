1a84cb340192c9cca75f734f9da6117d77a943b6
Dina Gomes <dinafgomes@gmail.com>
2016-02-15 22:38:00 +0000

    Labs_correction

---
+++ SDPM_Labs/ThreeDShape.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+namespace Shape
+{
+	public abstract class ThreeDShape
+	{
+        private String type;
+
+        public ThreeDShape(string type)
+        {
+            this.type = type;
+        }
+    }
+}
